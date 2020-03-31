34b2ac3073a91e72454f9a7d90300a71dfdd7d9b
leejisun <leejisun@leejisunui-MacBook-Pro.local>
2020-04-01 12:30:52 +1300

    ellacommit

--- src/components_ella/Home.jsx
+++ src/components_ella/Home.jsx
@@ -4,10 +4,31 @@ import * as React from "react";
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { cars: [] };
+  }
+
+  componentDidMount() {
+    Axios.get(`http://localhost:4000/api/cars`).then(
+      res => {
+        if (res.data.result === false) {
+          this.setState({ result: false });
+        } else {
+          this.setState({ cars: res.data, result: true });
+        }
+      },
+      error => {
+        console.log("error = ", error);
+      }
+    );
   }
 
   render() {
-    return <div>Featured cars</div>;
+    return (
+      <div>
+        <h1>Featured cars</h1>
+        {this.state.result === false ? <p>no users returned</p> : null}
+        <li>{this.props.car_image}</li>
+      </div>
+    );
   }
 }
